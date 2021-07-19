f9b353ce62b85097da9534f7f1cc0bfd46a3703b
choihyerim <hyerimc858@gmail.com>
2021-07-19 20:35:49 +0900

    Django C.R.U.D 2

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bow_wow',
+        'USER': 'root',
+        'PASSWORD': 'kim99108!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-ytesu$#a4h*@c*vyx(v@gqokj(8)3kbxgxqb^y4yuv*nevj5%6' 
+#settings.py에 있는 secret_key 를 사용합니다.
